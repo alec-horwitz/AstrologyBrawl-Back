0dbf632d764586f7bc362feb4a502a0b12853955
github username <github email address>
2018-04-17 15:32:06 -0400

    created create action in games

--- app/controllers/concerns/api/v1/games_controller.rb
+++ app/controllers/concerns/api/v1/games_controller.rb
@@ -5,4 +5,12 @@ class Api::V1::GamesController < ApplicationController
     render json: games
   end
 
+  def create
+    game = Game.create(game_params)
+  end
+
+  def game_params
+   params.require(:game).permit(:playername, :score)
+  end
+
 end
