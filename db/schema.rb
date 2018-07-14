2112923a6887a6b56abd26193ea11df9e32bc1eb
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-07-13 22:40:27 -0400

    convert db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180416175657) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.integer "user_id"
     t.string "playername"
