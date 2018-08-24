2ddcd167cda240ebad113ee7cccc79155f63178b
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-08-24 08:22:50 -0400

    fixed type missmatch with username param

--- app/controllers/concerns/api/v1/sessions_controller.rb
+++ app/controllers/concerns/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    @user = User.find_by(username: params["username"])
-    if (@user && @user.authenticate(params["password"]))
+    @user = User.find_by(username: params[:name])
+    if (@user && @user.authenticate(params[:password]))
       render json: token_json(@user)
     else
       render json: {
